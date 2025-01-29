68e32806fd801c88ffd468ffe8a6c5d281051d14
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 10:10:10 +0530

    Update ConditionalStatements.java

--- ConditionalStatements.java
+++ ConditionalStatements.java
@@ -1,14 +1,22 @@
 public class ConditionalStatements {
     public static void main(String[] args) {
-        int a=-2345;
-        if(a>0){
+        // Initializing
+        int a = -2345;
+
+        // Checking if the value of 'a' is positive
+        if(a > 0) {
+            // If 'a' is greater than 0, print that the number is positive
             System.out.println("the number is positive");
         }
-        else if(a<0){
+        // If 'a' is not positive, check if it's negative
+        else if(a < 0) {
+            // If 'a' is less than 0, print that the number is negative
             System.out.println("the number is negative");
         }
-        else 
-           System.out.println("its 0");
+        // If 'a' is neither positive nor negative, it must be 0
+        else {
+            // If 'a' is 0, print that it is 0
+            System.out.println("its 0");
+        }
     }
-    
 }
